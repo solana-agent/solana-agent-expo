f6362d4918eada7bc89ebf09c54aa378cadf1bbb
Bevan Hunt <bevan@bevanhunt.com>
2025-07-25 21:59:52 -0700

    update

--- app/channel/[cid].tsx
+++ app/channel/[cid].tsx
@@ -25,6 +25,8 @@ export default function ChannelScreen() {
     const [isBlocked, setIsBlocked] = useState(false);
 
     useEffect(() => {
+        setError('');
+        setLoading(true);
         const loadChannel = async () => {
             try {
                 if (!client || !cid) {
